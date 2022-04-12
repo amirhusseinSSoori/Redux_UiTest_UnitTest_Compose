1512a3ff2edf04c0fb29c9b777370f0819624db7
Amirhussein soori <amirhusseinsppro75@gmail.com>
2022-04-12 17:48:41 +0430

    create repository

---
+++ app/src/main/java/com/amirhusseinsoori/code_challenge/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.amirhusseinsoori.code_challenge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.amirhusseinsoori.code_challenge.ui.theme.Code_challengeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Code_challengeTheme {
+
+
+            }
+        }
+    }
+}
